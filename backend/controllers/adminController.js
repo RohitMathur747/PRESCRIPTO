c3b8386fec128ad605eaf9e168f421d92fd42715
Rohit Mathur <127666041+RohitMathur747@users.noreply.github.com>
2026-03-24 23:14:12 +0530

    backend controller.models,routes

---
+++ backend/controllers/adminController.js
@@ -0,0 +1,34 @@
+// api for adding doctors
+
+const addDoctors = async (req, res) => {
+  try {
+    const {
+      name,
+      email,
+      password,
+      speciality,
+      degree,
+      experience,
+      about,
+      fees,
+      address,
+    } = req.body;
+    const imageFile = req.file;
+    console.log({
+      name,
+      email,
+      password,
+      speciality,
+      degree,
+      experience,
+      about,
+      fees,
+      address,
+      imageFile,
+    });
+  } catch (error) {
+    console.log("error");
+  }
+};
+
+export { addDoctors };
--- backend/server.js
+++ backend/server.js
@@ -3,16 +3,24 @@ import express from "express";
 import cors from "cors";
 import "dotenv/config";
 import connectDB from "./config/mongodb.js";
+import connectCloudinary from "./config/cloudinary.js";
+import adminRouter from "./routes/adminRoute.js";
 
 //app config
 const app = express();
 const port = process.env.PORT || 4000; // Connect to DB\n(async () => {\n  try {\n    await connectDB();\n  } catch (err) {\n    console.error('DB connection error:', err);\n    process.exit(1);\n  }\n})();
+connectDB();
+connectCloudinary();
 
 //middleware
 app.use(express.json());
 app.use(cors());
 
 //api endpoints
+
+app.use("/api/admin", adminRouter);
+//localhost:4000/api/admin
+
 app.get("/", (req, res) => {
   res.send("Api Working");
 });

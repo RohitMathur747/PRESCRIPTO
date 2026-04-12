b51b81fa3b66a9112a124c571522b7e2288c9dd5
Rohit Mathur <127666041+RohitMathur747@users.noreply.github.com>
2026-04-12 13:10:27 +0530

    Doctor Appointments Functionality for Doctor Dashboard Panel

---
+++ Admin/src/pages/Doctor/DoctorDashboard.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const DoctorDashboard = () => {
+  return (
+    <div>
+      <div>Doctor Dashboard</div>
+    </div>
+  );
+};
+
+export default DoctorDashboard;
--- backend/routes/doctorRoute.js
+++ backend/routes/doctorRoute.js
@@ -1,8 +1,18 @@
 import express from "express";
-import { doctorList, LoginDoctor } from "../controllers/doctorController.js";
+import {
+  doctorList,
+  LoginDoctor,
+  appointmentsDoctor,
+  appointmentCompleted,
+  appointmentCancel,
+} from "../controllers/doctorController.js";
+import authDoctor from "../middleware/authDoctor.js";
 const doctorRouter = express.Router();
 
 doctorRouter.get("/list", doctorList);
 doctorRouter.post("/login", LoginDoctor);
+doctorRouter.post("/appointments", authDoctor, appointmentsDoctor);
+doctorRouter.post("/complete-appointment", authDoctor, appointmentCompleted);
+doctorRouter.post("/cancel-appointment", authDoctor, appointmentCancel);
 
 export default doctorRouter;
